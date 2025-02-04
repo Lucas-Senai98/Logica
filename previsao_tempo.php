2bb3187ff2f2371b2f9aaed51da4c125e8776b81
Lucas Fraga Pinho Carvalho <BPI0060128@aluno.firjansenaisesi.com.br>
2025-02-04 19:30:30 -0300

    Nova funcionalidade previsão do tempo

---
+++ previsao_tempo.php
@@ -0,0 +1,18 @@
+<?php
+
+$Tempertura = 30;
+
+
+if ($Temperatura >= 30) 
+{
+
+    echo $Tempertura .  ' O dia está nublado ensolarado.';
+
+}
+
+
+
+else{
+
+    echo $Tempertura . ' O dia está nublado ';    
+}
